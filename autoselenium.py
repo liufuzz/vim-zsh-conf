095772e3b0fd08c4a07ef05da76156188cbadf0c
liufu <liufuzz@163.com>
2019-06-24 14:18:07 +0800

    Update autoselenium.py
    init

--- autoselenium.py
+++ autoselenium.py
@@ -4,12 +4,15 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://arknights.ml/user")
 
-driver.find_element_by_id("email").send_keys("liufu9935@gmail.com")
-driver.find_element_by_id("passwd").send_keys("mn123456")
+driver.find_element_by_id("email").send_keys("***")
+driver.find_element_by_id("passwd").send_keys("***")
 driver.find_element_by_id("login").click()
 time.sleep(2)
-#driver.find_element_by_class_name("btn-flat").click()
-#time.sleep(2)
+try:
+  driver.find_element_by_id("checkin").click()
+except Exception:
+  print('已经签过了')
+time.sleep(2)
 result = driver.find_elements_by_class_name("btn-flat")[1].text
 print(result)
 driver.quit()
